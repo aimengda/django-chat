ac4def489d6a568eacfd5dec3f159e47025365a6
wangmengda <wangmengda@i.shu.edu.cn>
2020-08-08 23:03:59 +0800

    format settings.py

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangochat.wsgi.application'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
